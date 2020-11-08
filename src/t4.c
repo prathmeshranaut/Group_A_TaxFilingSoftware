52aae0ef4b05722635cbe8ff071382d6dc56bccb
Aayush Ranaut <aayush.ranaut@gmail.com>
2020-11-08 12:35:55 -0500

    Fixed bug with scanf in T4 forms

--- src/t4.c
+++ src/t4.c
@@ -1,13 +1,13 @@
 #include "headers/t4.h"
-#include <stdio.h>
-#include "cJSON.h"
 
 int input_t4_details(cJSON *root) {
     char has_details = 'y';
-    cJSON *t4_details_array = cJSON_CreateArray();
 
     printf("Do you wish to enter T4 details? (y/n)");
-    scanf("%c", &has_details);
+    scanf(" %c", &has_details);
+    printf("Done waiting");
+
+    cJSON *t4_details_array = cJSON_CreateArray();
 
     while ('y' == has_details) {
         t4 t4_details;
@@ -65,27 +65,27 @@ int input_t4_details(cJSON *root) {
 
         cJSON *t4_details_object = cJSON_CreateObject();
 
-        cJSON_AddItemToObject(t4_details_object, EMPLOYER_NAME, cJSON_CreateString(t4_details.employers_name));
+        cJSON_AddItemToObject(t4_details_object, EMPLOYER_NAME, cJSON_CreateString(t4_details.employer_name));
         cJSON_AddItemToObject(t4_details_object, PROVINCE, cJSON_CreateString(t4_details.province));
-        cJSON_AddItemToObject(t4_details_object, EMPLOYMENT_INCOME, cJSON_CreateString(t4_details.employment_income));
-        cJSON_AddItemToObject(t4_details_object, EMPLOYEES_CPP_CONTRIBUTION, cJSON_CreateString(t4_details.employees_cpp_contribution));
-        cJSON_AddItemToObject(t4_details_object, EMPLOYEES_QPP_CONTRIBUTION, cJSON_CreateString(t4_details.employees_qpp_contribution));
-        cJSON_AddItemToObject(t4_details_object, EMPLOYEES_EI_PREMIUMS, cJSON_CreateString(t4_details.employees_ei_premiums));
-        cJSON_AddItemToObject(t4_details_object, RPP_CONTRIBUTIONS, cJSON_CreateString(t4_details.rpp_contributions));
-        cJSON_AddItemToObject(t4_details_object, INCOME_TAX_DEDUCTED, cJSON_CreateString(t4_details.income_tax_deducted));
-        cJSON_AddItemToObject(t4_details_object, EI_INSURABLE_EARNINGS, cJSON_CreateString(t4_details.ei_insurable_earnings));
-        cJSON_AddItemToObject(t4_details_object, CPP_QPP_PENSIONABLE_EARNINGS, cJSON_CreateString(t4_details.cpp_qpp_pensionable_earnings));
-        cJSON_AddItemToObject(t4_details_object, UNION_DUES, cJSON_CreateString(t4_details.union_dues));
-        cJSON_AddItemToObject(t4_details_object, CHARITABLE_DONATIONS, cJSON_CreateString(t4_details.charitable_donations));
-        cJSON_AddItemToObject(t4_details_object, RPP_DSSP_REGISTRATION_NUMBER, cJSON_CreateString(t4_details.rpp_dssp_registation_number));
-        cJSON_AddItemToObject(t4_details_object, PENSION_ADJUSTMENT, cJSON_CreateString(t4_details.pension_adjustment));
-        cJSON_AddItemToObject(t4_details_object, EMPLOYEES_PPIP_PREMIUMS, cJSON_CreateString(t4_details.employees_ppip_premiums));
-        cJSON_AddItemToObject(t4_details_object, PPIP_INSURABLE_EARNINGS, cJSON_CreateString(t4_details.ppip_insurable_earnings));
+        cJSON_AddItemToObject(t4_details_object, EMPLOYMENT_INCOME, cJSON_CreateNumber(t4_details.employment_income));
+        cJSON_AddItemToObject(t4_details_object, EMPLOYEES_CPP_CONTRIBUTION, cJSON_CreateNumber(t4_details.employees_cpp_contribution));
+        cJSON_AddItemToObject(t4_details_object, EMPLOYEES_QPP_CONTRIBUTION, cJSON_CreateNumber(t4_details.employees_qpp_contribution));
+        cJSON_AddItemToObject(t4_details_object, EMPLOYEES_EI_PREMIUMS, cJSON_CreateNumber(t4_details.employees_ei_premiums));
+        cJSON_AddItemToObject(t4_details_object, RPP_CONTRIBUTIONS, cJSON_CreateNumber(t4_details.rpp_contributions));
+        cJSON_AddItemToObject(t4_details_object, INCOME_TAX_DEDUCTED, cJSON_CreateNumber(t4_details.income_tax_deducted));
+        cJSON_AddItemToObject(t4_details_object, EI_INSURABLE_EARNINGS, cJSON_CreateNumber(t4_details.ei_insurable_earnings));
+        cJSON_AddItemToObject(t4_details_object, CPP_QPP_PENSIONABLE_EARNINGS, cJSON_CreateNumber(t4_details.cpp_qpp_pensionable_earnings));
+        cJSON_AddItemToObject(t4_details_object, UNION_DUES, cJSON_CreateNumber(t4_details.union_dues));
+        cJSON_AddItemToObject(t4_details_object, CHARITABLE_DONATIONS, cJSON_CreateNumber(t4_details.charitable_donations));
+        cJSON_AddItemToObject(t4_details_object, RPP_DSSP_REGISTRATION_NUMBER, cJSON_CreateNumber(t4_details.rpp_dssp_registation_number));
+        cJSON_AddItemToObject(t4_details_object, PENSION_ADJUSTMENT, cJSON_CreateNumber(t4_details.pension_adjustment));
+        cJSON_AddItemToObject(t4_details_object, EMPLOYEES_PPIP_PREMIUMS, cJSON_CreateNumber(t4_details.employees_ppip_premiums));
+        cJSON_AddItemToObject(t4_details_object, PPIP_INSURABLE_EARNINGS, cJSON_CreateNumber(t4_details.ppip_insurable_earnings));
 
         cJSON_AddItemToArray(t4_details_array, t4_details_object);
 
         printf("Do you wish to enter more T4 details? (y/n)");
-        scanf("%c", &has_details);
+        scanf(" %c", &has_details);
     }
 
     cJSON_AddItemToObject(root, T4_KEY, t4_details_array);
