d63aca8ce74e5ea26c5e7a30bcaf0831222012f0
Aayush Ranaut <aayush.ranaut@gmail.com>
2020-11-13 19:16:55 -0500

    Fixed a test

--- test/src/t4.c
+++ test/src/t4.c
@@ -5,6 +5,7 @@
 void test_input_t4_details_aborts_if_root_passed_is_null() {
     cJSON* root = NULL;
     stdin = freopen("test/data/t4_1.txt", "r", stdin);
+
     TEST_ABORT();
     input_t4_details(root);
     fclose(stdin);
