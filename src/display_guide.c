a9dd185721f92018a67b803afd0b1b5277bd9698
RaNa1 <buzzragu@gmail.com>
2020-11-19 04:38:14 -0500

    Added new functions - calculate_tax and create_new_taxfile; renamed and modified display_guide and input_mailing_address functions; removed inital functions userDetails and guide; Added header files for new fucntions and old functions.

---
+++ src/display_guide.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "cJSON.h"
+
+int display_file(char* filename)
+{
+	FILE* fptr;
+
+	char c;
+	printf("file name is %s\n", filename);
+	// Open file 
+	fptr = fopen(filename, "r");
+	if (fptr == NULL)
+	{
+		printf("\t\tCannot open file \n");
+		exit(0);
+	}
+
+	// Read contents from file 
+	c = fgetc(fptr);
+	while (c != EOF)
+	{
+		printf("%c", c);
+		c = fgetc(fptr);
+	}
+
+	fclose(fptr);
+	return 0;
+}
+
+int display_guide(void)
+{
+	int attempt, max_attempt = 5;
+	char choice;
+	
+	printf("\t\t#######################################################################################\n");
+	printf("\n\n\t\t\t\t\t\tWelcome to Tax Filing software\n\n\n");
+	printf("\t\t#######################################################################################\n");
+
+guide:
+	attempt = 0;
+	do {
+		printf("\n\n\t\t\tUser Manual\n\n");
+		printf("\t\t\t\ta.Basic Tax filing\n\t\t\t\tb.Child Benifits\n\t\t\t\tc.Ontario Trillium Benifits\n\t\t\t\td.GST/HST Credits\n\t\t\t\te.Return to Main menu\n");
+		printf("You have %d attempts", max_attempt - attempt);
+		printf("\n\n\tEnter the choice : \n\t\t");
+		scanf("%c", &choice);
+		attempt++;
+		if (attempt > max_attempt)
+			goto exit;
+	} while (choice != 'a' && choice != 'b' && choice != 'c' && choice != 'd' && choice != 'e' && attempt <= max_attempt);
+
+	switch (choice) {
+	case 'a':
+
+		printf("\t\t\t\tBasic Tax filing\n\n\n");
+		display_file("./data/guide_basic.dat");
+		goto guide;
+		break;
+
+	case 'b':
+
+		printf("\t\t\t\tChild Benifits\n\n\n");
+		display_file("./data/guide_child.dat");
+		goto guide;
+		break;
+
+	case 'c':
+
+		printf("\t\t\t\tOntario Trillium Benifits\n\n\n");
+		display_file("./data/guide_otb.dat");
+		goto guide;
+		break;
+
+	case 'd':
+
+		printf("\t\t\t\tGST/HST Credits\n\n\n");
+		display_file("./data/guide_hst.dat");
+		goto guide;
+		break;
+
+	case 'e': 
+
+		printf(".....Returning to Main menu\n\n\n");
+
+		break;
+
+	default:
+
+		printf("\t\t\t\tInvalid Option\n\n\n");
+
+		goto guide;
+	}
+exit: 
+	return 0;
+}
--- src/main.c
+++ src/main.c
@@ -2,69 +2,75 @@
 #include "cJSON.h"
 #include "headers/primary_address.h"
 #include "headers/t4.h"
+#include "headers/mailing_address.h"
+#include "headers/new_taxfile.h"
+#include "headers/calculate_tax.h"
+#include "headers/entry_status.h"
+#include "headers/maritial_status.h"
+#include "headers/world_income.h"
 
-int displayGuide(void);
 
-cJSON *dofile(char *filename);
-
-void printJson(char *root);
-
-int input_new_TaxFile(cJSON *root);
 
 
 //void do_test(const char* test_name);
 //int show_files(void);
 int main(void) {
-    //Declaring and initializing variabless
-    int choice = 0, income = 0, tax = 0;
-    printf("\t\t#######################################################################################\n");
-    printf("\n\n\t\t\t\t\t\tWelcome to Tax Filing software\n\n\n");
-    printf("\t\t#######################################################################################\n");
-    menu:
-    printf("\n\n\t\t\tMain Menu\n\n");
-    printf("\t\t\t\t1.File New Tax\n\t\t\t\t2.Guide\n\t\t\t\t3.Exit\n");
+	//Declaring and initializing variabless
+	int attempt, max_attempt = 5;
+	char choice;
+	printf("\t\t#######################################################################################\n");
+	printf("\n\n\t\t\t\t\t\tWelcome to Tax Filing software\n\n\n");
+	printf("\t\t#######################################################################################\n");
+menu:
+	attempt = 0;
 
-    printf("\n\n\tEnter the choice : \n\t\t");
-    scanf("%d", &choice);
-    switch (choice) {
-        case 1 :
-            printf("\t\t\t\tFile New Tax\n\n\n");
-            //show_files();
-            //do_test("test1");
-            cJSON *root = cJSON_CreateObject();
-            input_new_TaxFile(root);
-//		mailing_address(root);
-            input_primary_residence(root);
-//		entry_status(root);
-//		world_income(root);
-//		maritial_status(root);
-            input_t4_details(root);
-//		otb_benefits(root);
-//		calculate_tax(root);
-            printJson(root);
-            //Write to a file
-            //create_NewTaxFile();
-            //dofile("./data/testJSON.json");
-            goto menu;
-            break;
-        case 2:
-            printf("\t\t\t\tGuide\n\n\n");
-            /* Guide menu */
-            displayGuide();
-            goto menu;
-            break;
-        case 3:
-            printf("\t\t\t\tExit\n\n\n");
-            goto exit;
-            break;
-        default:
-            printf("\t\t\t\tInvalid entry\n\n\n");
-            goto menu;
-    }
+	do {
+		printf("\n\n\t\t\tMain Menu\n\n");
+		printf("\t\t\t\ta.File New Tax\n\t\t\t\tb.Guide\n\t\t\t\tc.Exit\n");
+		printf("You have %d attempts", max_attempt - attempt);
+		printf("\n\n\tEnter the choice : \n\t\t");
+		scanf("%c", &choice);
+		attempt++;
+		if (attempt > max_attempt)
+			goto exit;
+	} while (choice != 'a' && choice != 'b' && choice != 'c' && attempt <= max_attempt);
+	switch (choice) {
+	case 'a':
+		printf("\t\t\t\tFile New Tax\n\n\n");
+		cJSON* root = cJSON_CreateObject();
+		input_new_taxfile(root);
+		input_mailing_address(root);
+		input_primary_residence(root);
+		input_entry_status(root);
+		input_world_income(root);
+		input_maritial_status(root);
+		input_t4_details(root);
+		//otb_benefits(root);
+		//calculate_tax(root);
+		print_json(root);
+		//Write to a file
+		create_new_taxfile(root);
+		goto menu;
+		break;
+	case 'b':
+		printf("\t\t\t\tGuide\n\n\n");
+		/* Guide menu */
+		display_guide();
+		goto menu;
+		break;
+	case 'c':
+		printf("\t\t\t\tExit\n\n\n");
+		goto exit;
+		break;
+	default:
+		printf("\t\t\t\tInvalid entry\n\n\n");
+		goto menu;
+	}
 
-    exit:
-    printf("\t\t#######################################################################################\n");
-    printf("\n\n\t\t\t\t\tThankyou for using Tax Filing Software\n\n\n");
-    printf("\t\t#######################################################################################\n");
-    return 0;
+
+exit:
+	printf("\t\t#######################################################################################\n");
+	printf("\n\n\t\t\t\t\tThankyou for using Tax Filing Software\n\n\n");
+	printf("\t\t#######################################################################################\n");
+	return 0;
 }
