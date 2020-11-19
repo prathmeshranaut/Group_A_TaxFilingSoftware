be53b2e235e6d350ce9a281318c473d6019cf101
RaNa1 <buzzragu@gmail.com>
2020-11-19 13:09:28 -0500

    Updating calculate_tax fucntion to get values from JSON object

--- src/calculate_tax.c
+++ src/calculate_tax.c
@@ -2,30 +2,22 @@
 #include <string.h>
 #include <stdlib.h>
 #include "headers/calculate_tax.h"
+#include "../../src/headers/t4.h"
+
 
 int calculate_tax(cJSON* root)
 {
+	cJSON* t4_object = cJSON_GetObjectItem(root, T4_KEY);
+	cJSON* first_t4_details = cJSON_GetArrayItem(t4_object, 0);
 
-	cJSON* json,* json_t4,* json_employment_income,* json_income_tax_deducted;//,* json_employees_cpp_contribution,* json_employees_ei_premiums,* json_rpp_contributions,* json_charitable_donations
-	double c_employment_income; //14
-	//double c_employees_cpp_contribution; //16
-	//double c_employees_qpp_contribution; //17
-	//double c_employees_ei_premiums; // 18
-	//double c_rpp_contributions; //20
-	double c_income_tax_deducted; //22
-	//double c_ei_insurable_earnings; //24
-	//double c_cpp_qpp_pensionable_earnings; //26
-	//double c_union_dues; //44
-	//double c_charitable_donations; //46
-	//double c_rpp_dssp_registation_number; //50
-	//double c_pension_adjustment; //52
-	//double c_employees_ppip_premiums; //55
-	//double c_ppip_insurable_earnings; //56
-	double c_federal_tax	= 0;
-	double c_income_tax		= 0;
+	cJSON* json;
+	double c_employment_income = 0;
+	double c_income_tax_deducted = 0;
+	double c_federal_tax = 0;
+	double c_income_tax = 0;
 	double c_provencial_tax = 0;
-	double c_refund			= 0;
-	double c_payable		= 0;
+	double c_refund = 0;
+	double c_payable = 0;
 
 	tax worker;
 
@@ -36,35 +28,14 @@ int calculate_tax(cJSON* root)
 		return -1;
 	}
 
-	json_t4 = cJSON_GetObjectItem(root, "t4");
-	
-	json_employment_income =  cJSON_GetObjectItem(json_t4, "employment_income");
-	c_employment_income = json_employment_income->valueint;
-	//json_employees_cpp_contribution =  cJSON_GetObjectItem(json_t4, "employees_cpp_contribution");
-	//c_employees_cpp_contribution = json_employees_cpp_contribution->valueint;
-	//json_employees_ei_premiums =  cJSON_GetObjectItem(json_t4, "employees_ei_premiums");
-	//c_employees_ei_premiums = json_employees_ei_premiums->valueint;
-	//json_employees_ei_premiums =  cJSON_GetObjectItem(json_t4, "employees_ei_premiums");
-	//c_employees_ei_premiums = json_employees_ei_premiums->valueint;
-	//json_rpp_contributions =  cJSON_GetObjectItem(json_t4, "rpp_contributions");
-	//c_rpp_contributions = json_rpp_contributions->valueint;
-	json_income_tax_deducted =  cJSON_GetObjectItem(json_t4, "income_tax_deducted");
-	c_income_tax_deducted = json_income_tax_deducted->valueint;
-	//json_charitable_donations =  cJSON_GetObjectItem(json_t4, "charitable_donations");
-	//c_charitable_donations = json_charitable_donations->valueint;
-	/*
-	c_employment_income = 98000.00;
-	c_employees_cpp_contribution = 2000.00;
-	c_employees_ei_premiums = 860.22;
-	c_rpp_contributions = 320;
-	c_income_tax_deducted = 230;
-	c_charitable_donations = 200;
-	*/
+	c_employment_income = cJSON_GetObjectItem(first_t4_details, EMPLOYMENT_INCOME)->valuedouble;
+	c_income_tax_deducted = cJSON_GetObjectItem(first_t4_details, INCOME_TAX_DEDUCTED)->valuedouble;
+
 	if (c_income_tax <= 0)
 	{
 		if (c_employment_income <= 47630.00)
 		{
-			c_federal_tax	= 0.15 * c_employment_income;
+			c_federal_tax = 0.15 * c_employment_income;
 		}
 		else if (c_employment_income > 47630.00 && c_employment_income <= 95259.00)
 		{
@@ -115,10 +86,12 @@ int calculate_tax(cJSON* root)
 
 		else if (c_income_tax > 12069)
 		{
-			c_payable = c_income_tax-12069;
+			c_payable = c_income_tax - 12069;
 		}
 
 	}
+
+	char* out = NULL;
 
 	if (!root)
 	{
@@ -153,5 +126,5 @@ int calculate_tax(cJSON* root)
 		cJSON_AddItemToObject(root, "tax", obj_person);
 
 	}
-	return 0;
+
 }
