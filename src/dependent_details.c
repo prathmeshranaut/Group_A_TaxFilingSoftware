7682bad545c170e796aedf613c340d60e498202b
Tirth Vyas <71532890+tirth31@users.noreply.github.com>
2020-11-30 15:47:35 -0500

    Child Care

--- src/dependent_details.c
+++ src/dependent_details.c
@@ -1,6 +1,6 @@
-#include "dependent_details.h"
+#include "headers/dependent_details.h"
 
-int input_dependant_details(cJSON *root) {
+int input_dependent_details(cJSON *root) {
      dependent_details details;
      printf("\n#######################################################################################\n");
      printf("Enter your Dependent Details\n");
@@ -12,26 +12,20 @@ int input_dependant_details(cJSON *root) {
      printf("Enter your Relationship:");
      scanf("%s", details.relationship);
 
-     printf("Enter your Dependent Date of birth(MM-DD-YYYY)");
+     printf("Enter your Dependent Date of birth(YYYY-MM-DD)");
      scanf("%s", details.dependent_dob);
 
      printf("Enter your Net Income:");
      scanf("%lf", &details.net_income);
-
-     printf("Enter your status of  Disability (Yes or No):");
-     scanf("%s", details.disability);
-
-     printf("Would you like to claim for child care benefits? (Yes or No):");
-     scanf("%s", details.claim_child_care_benefits);
+   
 
      cJSON *dependent_details_object = cJSON_CreateObject();
 
      cJSON_AddItemToObject(dependent_details_object, DEPENDENT_NAME, cJSON_CreateString(details.dependent_name));
      cJSON_AddItemToObject(dependent_details_object, RELATIONSHIP, cJSON_CreateString(details.relationship));
      cJSON_AddItemToObject(dependent_details_object, DEPENDENT_DOB, cJSON_CreateString(details.dependent_dob));
-     cJSON_AddItemToObject(dependent_details_object, NET_INCOME, cJSON_CreateString(details.net_income));
-     cJSON_AddItemToObject(dependent_details_object, DISABILITY, cJSON_CreateString(details.disability));
-     cJSON_AddItemToObject(dependent_details_object, CLAIM_CHILD_CARE_BENEFITS, cJSON_CreateString(details.claim_child_care_benefits));
+     cJSON_AddItemToObject(dependent_details_object, NET_INCOME, cJSON_CreateNumber(details.net_income));
+     
 
      cJSON_AddItemToObject(root, DEPENDENT_DETAILS_KEY, dependent_details_object);
 
