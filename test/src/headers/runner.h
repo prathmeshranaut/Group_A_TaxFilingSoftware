a67d0e850935c3438671c20b2d74e94f83a2bb1a
shreya9898 <32429964+shreya9898@users.noreply.github.com>
2020-12-05 20:55:51 -0500

    Update runner.h

--- test/src/headers/runner.h
+++ test/src/headers/runner.h
@@ -7,6 +7,23 @@ extern void test_input_mailing_address_details_add_primary_address_key_to_json()
 extern void test_input_mailing_address_details_correctly_and_returns();
 extern void test_input_mailing_address_correctly_adds_value_for_fields();
 
+//Entry Status Tests
+extern void test_input_entry_status_aborts_if_root_passed_is_null();
+extern void test_input_entry_status_details_add_entry_status_key_to_json();
+extern void test_input_entry_status_details_correctly_and_returns();
+extern void test_input_entry_status_correctly_adds_value_for_fields();
+
+//Maritial Status Tests
+extern void test_input_maritial_status_aborts_if_root_passed_is_null();
+extern void test_input_maritial_status_details_add_maritial_status_key_to_json();
+extern void test_input_maritial_status_details_correctly_and_returns();
+extern void test_input_maritial_status_correctly_adds_value_for_fields();
+
+//World Income Tests
+extern void test_input_world_income_aborts_if_root_passed_is_null();
+extern void test_input_world_income_details_add_world_income_key_to_json();
+extern void test_input_world_income_details_correctly_and_returns();
+extern void test_input_world_income_correctly_adds_value_for_fields();
 
 //Primary Address Tests
 extern void test_input_primary_address_aborts_if_root_passed_is_null();
