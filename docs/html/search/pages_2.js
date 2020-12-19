6ac2b66b56eb758c99153c893bbd12e68b5cb19d
Aayush Ranaut <aayush.ranaut@gmail.com>
2020-12-19 13:59:54 -0500

    Fixed makefile

--- docs/html/search/pages_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['tax_5ffiling_5fsoftware_48',['Tax_Filing_Software',['../md__r_e_a_d_m_e.html',1,'']]]
-];
--- docs/html/search/pages_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['user_5fmanual_49',['User_Manual',['../md_doc__user__manual.html',1,'']]]
-];
